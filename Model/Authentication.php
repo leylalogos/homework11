c62663cb1364c54a16e2ce14723d2425e0f02f8b
leyla sabouri <dev.leyla.sabouri@gmail.com>
2021-09-10 11:37:53 +0430

    hashed password

--- Model/Authentication.php
+++ Model/Authentication.php
@@ -5,8 +5,9 @@ class Authentication
 {
 	function createUser($username, $mobile, $email, $password)
 	{
+		$passwordHash = password_hash($password, PASSWORD_DEFAULT);
 		$connection = ConnectToDatabase();
-		$sql = "INSERT INTO useres(username,mobile,email,password,credit,is_active)VALUES ('$username','$mobile','$email','$password',0,1)";
+		$sql = "INSERT INTO useres(username,mobile,email,password,credit,is_active)VALUES ('$username','$mobile','$email','$passwordHash',0,1)";
 
 		if ($connection->query($sql) === TRUE) {
 			echo "New record created successfully";
